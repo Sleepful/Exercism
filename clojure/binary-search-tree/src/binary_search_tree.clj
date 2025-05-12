92bdc4a2b2c8459266d129fd7a59de821d348608
Sleepful <josepablov@gmail.com>
2025-05-11 21:39:45 -0600

    binary tree to ordered list

--- clojure/binary-search-tree/src/binary_search_tree.clj
+++ clojure/binary-search-tree/src/binary_search_tree.clj
@@ -30,7 +30,28 @@
 (defn right [node]
   (:right node))
 
-(defn to-list [node])
+(defn to-list [root]
+  (loop [current-node root
+         backtrack-stack [root]
+         ordered-values []
+         backtrack false]
+    #_(prn (:value current-node) backtrack-stack ordered-values)
+    (cond
+      (and (not backtrack) (:left current-node))
+      (recur (:left current-node) (conj backtrack-stack (:left current-node)) ordered-values false)
+
+      (:right current-node)
+      (recur (:right current-node) (conj (pop backtrack-stack) (:right current-node)) (conj ordered-values (:value current-node)) false)
+
+      (and current-node (seq (pop backtrack-stack))) ; leaf node, with option to backtrack
+      (recur
+       (peek (pop backtrack-stack))
+       (pop backtrack-stack)
+       (conj ordered-values (:value current-node))
+       true)
+
+      current-node ; last leaf node
+      (conj ordered-values (:value current-node)))))
 
 (defn from-list [[first & rest :as _values]]
   (reduce (fn [acc new] (insert new acc)) (singleton first) rest))
